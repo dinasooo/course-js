a795927620c7c83a81609d0666d55acdcb1e237e
Kayumova Dinara <dinara.ara79@gmail.com>
2023-04-22 17:07:28 +0300

    первое дз

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,7 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
+function returnFirstArgument(a) {
+  return a;
+}
 
 /*
  Задание 2:
@@ -22,13 +24,20 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10, 20) вернет 30
    sumWithDefaults(2, 4) вернет 6
-
+  */ 
+// 
+// function sumWithDefaults(a,b) {
+//   return a+b;
+// }
+/*
  2.2 *: Значение по умолчанию для второго аргумента должно быть равно 100
 
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
+function sumWithDefaults(a,b=100) {
+  return a+b;
+}
 
 /*
  Задание 3:
@@ -38,7 +47,9 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
+function returnFnResult(fn) {
+  return fn();
+}
 
 /*
  Задание 4:
@@ -53,7 +64,9 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
+function returnCounter(a = 0) {
+  return () => ++a;
+}
 
 /*
  Задание 5 *:
@@ -64,7 +77,9 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...args) {
+  return args; 
+}
 
 export {
   returnFirstArgument,
