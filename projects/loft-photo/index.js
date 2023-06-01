41a9a9efddf86d55cac6f221ebd8a53422ada778
Kayumova Dinara <dinara.ara79@gmail.com>
2023-06-01 19:41:02 +0300

    дз 4,5,6

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,10 +1,21 @@
 import pages from './pages.js';
-import model from './model.js';
-import('./styles.css');
+// import model from './model.js';
+// import('./styles.css');
 
-const pageNames = ['login', 'main', 'profile'];
+// const pageNames = ['login', 'main', 'profile'];
 
-document.addEventListener('click', () => {
-    const pageName = model.getRandomElement(pageNames);
-    pages.openPage(pageName);
-});
+// document.addEventListener('click', () => {
+//     const pageName = model.getRandomElement(pageNames);
+//     pages.openPage(pageName);
+// });
+
+import mainPage from './mainPage';
+import loginPage from './loginPage';
+import profilePage from './profilePage';
+
+pages.openPage('login');
+loginPage.handleEvents();
+mainPage.handleEvents();
+profilePage.handleEvents();
+
+
--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -1,25 +1,166 @@
-// eslint-disable-next-line no-unused-vars
-import photosDB from './photos.json';
-// eslint-disable-next-line no-unused-vars
-import friendsDB from './friends.json';
+import { resolve } from "path";
 
 export default {
   getRandomElement(array) {
-    if(!array.length){
+    if (!array.length) {
       return null;
     }
 
-    const index = Math.round(Math.random() * (array.length -1));
+    const index = Math.round(Math.random() * (array.length - 1));
 
     return array[index];
 
   },
 
-  getNextPhoto() {
-    const friend = this.getRandomElement(friendsDB);
-    const photos = photosDB[friend.id];
-    const photo = this.getRandomElement(photos);
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
 
-    return{friend, url: photo.url}
+    return { friend, id: photo.id, url: size.url };
   },
-}
+
+  findSize(photo) {
+    const size = photo.sizes.find((size) => size.width >= 360);
+
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) => {
+        if (current.width > biggest.width) {
+          return current;
+        }
+
+        return biggest;
+      }, photo.sizes[0]);
+    }
+
+    return size;
+  },
+
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFriendss();
+  },
+
+  login() {
+    return new Promise((resolve, reject) => {
+      VK.init({
+        apiId: APP_ID,
+      });
+
+      VK.Auth.login((response) => {
+        if (response.session) {
+          this.toke = response.session.sid;
+          resolve(response);
+        } else {
+          console.error(response);
+          reject(response);
+        }
+      }, PERM_FRIENDS | PERM_PHOTOS);
+    });
+  },
+
+  logout() {
+    return new Promise((resolve) => VK.Auth.revokeGrants(resolve));
+  },
+
+  getUsers(ids) {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    if (ids) {
+      params.user_ids = ids;
+    }
+
+    return this.callApi('users.get', params);
+  },
+
+  callApi(method, params) {
+    params.v = params.v || '5.120';
+
+    return new Promise((resolve, reject) => {
+      VK.api(method, params, (response) => {
+        if (response.error) {
+          reject(new Error(response.error.error_nsg));
+        } else {
+          resolve(response.response);
+        }
+      });
+    });
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id: owner,
+    };
+
+    return this.callApi('photos.getAll', params);
+  },
+
+  async getFriendPhotos(id) {
+    let photos = this.photoCache[id];
+
+    if (photos) {
+      return photos;
+    }
+
+    photos = await this.getPhotos(id);
+
+    this.photoCache[id] = photos;
+
+    return photos;
+  },
+
+  async callServer(method, queryParams, body){
+    queryParams = {
+      ...queryParams,
+      method,
+    };
+
+    const query = Object.entries(queryParams).reduce((all, [name, value]) => {
+      all.push(`${name}=${encodeURIComponent(value)}`);
+      return all;
+    }, [])
+    .join('&');
+    const params = {
+      headers: {
+        vk_token: this.token,
+      },
+    };
+
+    if(body){
+      params.method = 'POST';
+      params.body = JSON.stringify(body);
+    }
+
+    const response = await fetch(`/loft-photo-lite-5/api/?${query}`, params);
+
+    return response.json();
+  },
+
+  async like(photo) { 
+    return this.callServer('like', {photo});
+  },
+
+  async photoStats(photo) {
+    return this.callServer('photoStats', {photo});
+   },
+
+  async getComments(photo) { 
+    return this.callServer('getComments', {photo});
+  },
+
+  async postComment(photo, text) { 
+    return this.callServer('postComment', {photo}, {text});
+  },
+
+};
